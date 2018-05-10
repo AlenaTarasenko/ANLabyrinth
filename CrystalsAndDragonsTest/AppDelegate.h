efe7d5e765e2705b37cf7b81ce6877a39398b8ab
Admin <admin@Mac-Admin.local>
2018-05-10 08:10:06 +0300

    Initial Commit

---
+++ CrystalsAndDragonsTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CrystalsAndDragonsTest
+//
+//  Created by Admin on 10.05.18.
+//  Copyright © 2018 Alena Tarasenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CrystalsAndDragonsTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CrystalsAndDragonsTest
+//
+//  Created by Admin on 10.05.18.
+//  Copyright © 2018 Alena Tarasenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
